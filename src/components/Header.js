cb28c26f7464994544d2ea4f37a160818cd72759
Tae Lee <taebumlee@gmail.com>
2019-09-22 12:09:40 -0700

    Header in function component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
+function Header () {
     return (
       <h1>BREWVANA</h1>
     )
-  }
 }
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <h1>BREWVANA</h1>
+//     )
+//   }
+// }
 
 export default Header;

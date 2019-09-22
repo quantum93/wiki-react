dd5c4f4c2d8f678d8fba86269a4f795fd2c1b709
Tae Lee <taebumlee@gmail.com>
2019-09-22 12:43:49 -0700

    It adds react-router-dom

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'));

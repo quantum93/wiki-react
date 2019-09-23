fdb57b0dde8b2d72d6edc4cde9c6df1ce8564f30
Tae Lee <taebumlee@gmail.com>
2019-09-22 19:08:31 -0700

    change to class component from functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Header from './Header';
 import {Route, Switch} from "react-router-dom";
 
-function App() {
+class App extends React.component {
+  constructor() {
+    super();
+    this.state = {
+      numBeers: 10
+    };
+  }
+
   return (
     <div className="App">
-      <Header siteName="BREWVANA"/>
-      <p>Hello World</p>
+    <Header siteName="BREWVANA"/>
+    <p>Hello World</p>
     </div>
   );
 }

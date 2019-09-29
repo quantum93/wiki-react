3c4edf417b9a8cf5e87eb8a90b8fb64aaa28221b
Tae Lee <taebumlee@gmail.com>
2019-09-29 09:00:45 -0700

    add search page on main screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Search from './Search';
 
 class App extends React.Component {
   constructor() {
@@ -31,7 +32,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      <Header siteName="BREWVANA"/>
+      <Header siteName="BREWVANA" />
+      <Search />
       <button onClick={this.incrementBeers}>{this.state.numBeers} 🍺</button>
       <p>Hello World</p>
       </div>

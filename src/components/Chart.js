3e6ce841fc2c789c3268b6feffb9e658fc649e1a
Student <example@example.com>
2019-10-01 11:03:38 -0700

    adds bar chart model in functional component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import db from './db.json';
+import {Bar} from 'react-chartjs-2';
 
-const Chart = () => {
-
-  let i;
-  console.log(db.data.length-1);
-  for (i = 0; i < db.data.length; i++) {
-    if (db.data[i].abv) {
-      console.log(i, db.data[i].abv);
-      console.log(db.data[i].ibu);
-      let style = db.data[i].style
-      console.log(i, style ? style.category.name : "no category name");
+const state = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56]
     }
-  }
+  ]
+}
 
-  return (
+const myChart = () => {
+  return(
     <div>
-      <h1>This is Chart page.</h1>
+    <Bar
+    data={state}
+    options={{
+      title:{
+        display:true,
+        text:'Average Rainfall per month',
+        fontSize:20
+      },
+      legend:{
+        display:true,
+        position:'right'
+      }
+    }}
+    />
     </div>
-  )
-
+  );
 
 }
 
-export default Chart;
+export default myChart;
+// let i;
+// console.log(db.data.length-1);
+// for (i = 0; i < db.data.length; i++) {
+  //   if (db.data[i].abv) {
+    //     console.log(i, db.data[i].abv);
+    //     console.log(db.data[i].ibu);
+    //     let style = db.data[i].style
+    //     console.log(i, style ? style.category.name : "no category name");
+    //   }
+    // }
